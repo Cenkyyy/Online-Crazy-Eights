fda822768ae25dfdf1981b845c3b6a71bf8d6a95
Oliver Tomáš Cenker <olivercenker4@gmail.com>
2025-02-19 23:21:28 +0100

    Prepare boilercode functions for model-type classes

--- Online Crazy Eights/src/main/java/cz/cuni/mff/java/project/Deck.java
+++ Online Crazy Eights/src/main/java/cz/cuni/mff/java/project/Deck.java
@@ -1,4 +1,35 @@
 package cz.cuni.mff.java.project;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
+    //
+    // Model
+    //
+
+    private ArrayList<Card> deckCards;
+
+    public Deck(){
+        deckCards = new ArrayList<>();
+        for (var suit : Card.Suits.values()){
+            for (var rank : Card.Ranks.values()){
+                deckCards.add(new Card(suit, rank));
+            }
+        }
+        ShuffleCards();
+    }
+
+    public void ShuffleCards(){
+        Collections.shuffle(deckCards);
+    }
+
+    public void AddCards(ArrayList<Card> cards){
+        deckCards.addAll(cards);
+        ShuffleCards();
+    }
+
+    public Card DrawCard(){
+        return deckCards.removeFirst();
+    }
 }
--- Online Crazy Eights/src/main/java/cz/cuni/mff/java/project/Player.java
+++ Online Crazy Eights/src/main/java/cz/cuni/mff/java/project/Player.java
@@ -1,4 +1,38 @@
 package cz.cuni.mff.java.project;
 
+import java.util.ArrayList;
+
 public class Player {
+    //
+    // Model
+    //
+
+    private ArrayList<Card> handCards;
+    private int points = 0;
+    private final String name;
+
+    public Player(String name){
+        this.name = name;
+        handCards = new ArrayList<>();
+    }
+
+    public int GetPoints(){
+        return points;
+    }
+
+    public void AddPoints(int amount){
+        points += amount;
+    }
+
+    public String GetName(){
+        return name;
+    }
+
+    public void AddCard(Card card){
+        handCards.add(card);
+    }
+
+    public void RemoveCard(Card card){
+        handCards.remove(card);
+    }
 }
